579478de380eac23b9f427817ad01cac24c6f9cc
asbdu281cb21s206017 <149135568+asbdu281cb21s206017@users.noreply.github.com>
2023-10-30 08:49:26 -0700

    Add files via upload

---
+++ muthu.css
@@ -0,0 +1,65 @@
+/* Reset some default styles */
+body, h1, h2, h3, p {
+    margin: 0;
+    padding: 0;
+}
+
+body {
+    font-family: Arial, sans-serif;
+    background-color: #f0f0f0;
+}
+
+header {
+    background-color: #333;
+    color: #fff;
+    padding: 20px;
+    text-align: center;
+}
+
+header h1 {
+    font-size: 32px;
+}
+
+nav ul {
+    list-style: none;
+}
+
+nav ul li {
+    display: inline;
+    margin-right: 20px;
+}
+
+nav ul li a {
+    text-decoration: none;
+    color: #fff;
+}
+
+section {
+    margin: 20px;
+    padding: 20px;
+    background-color: #fff;
+    border-radius: 5px;
+}
+
+.project {
+    margin: 20px 0;
+}
+
+.project img {
+    max-width: 100%;
+    height: auto;
+}
+
+.project h3 {
+    font-size: 20px;
+    margin: 10px 0;
+}
+
+/* Add more styles as needed */
+
+/* Media query for responsiveness */
+@media (max-width: 768px) {
+    header {
+        text-align: left;
+    }
+}
